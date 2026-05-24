d76402acfc4e1de2ef8f0af4153531d2d983a554
linhaipeng <hello@world>
2026-05-24 12:37:14 +0800

    员工相关代码

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,13 @@
 package com.sky.mapper;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 @Mapper
 public interface EmployeeMapper {
@@ -15,4 +20,20 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    //新增员工
+    @Insert("insert into employee(name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user)values (#{name},#{username},#{password},#{phone}" +
+            ",#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    public void insert(Employee employee);
+
+    public List<Employee> selectPage(String name);
+
+    @Update("update employee set status =#{status} where id=#{id}")
+    public void updateEmployee(Integer status,Integer id);
+
+    //显示回显
+    @Select("select * from employee where id=#{id}")
+    public Employee selectUser(Integer id);
+
+    //修改员工
+    public void update(Employee employee);
 }
